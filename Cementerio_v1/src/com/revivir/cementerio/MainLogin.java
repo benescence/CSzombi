54ba200fc126292dd719387c20129eec4f387e52
Carlos <carloscaballeromorel@gmail.com>
2018-12-04 03:56:16 -0300

    Se cambio la apariencia de la aplicacion con JTatoo.

--- Cementerio_v1/src/com/revivir/cementerio/MainLogin.java
+++ Cementerio_v1/src/com/revivir/cementerio/MainLogin.java
@@ -1,10 +1,25 @@
 package com.revivir.cementerio;
 
+import java.util.Properties;
+
+import javax.swing.UIManager;
+
+import com.jtattoo.plaf.acryl.AcrylLookAndFeel;
 import com.revivir.cementerio.vista.login.ControladorIniciarSesion;
 
 public class MainLogin {
 
 	public static void main(String[] args) {
+		try {
+			Properties props = new Properties();
+			props.put("logoString", "Revivir");
+			
+			AcrylLookAndFeel.setCurrentTheme(props);
+			UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		new ControladorIniciarSesion();
 	}
 
