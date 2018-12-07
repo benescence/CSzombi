405ecb856d7e0afee80c0e164e6675401e5ed4ec
Carlos <carloscaballeromorel@gmail.com>
2018-12-06 23:51:47 -0300

    Merge resultado.

--- Cementerio_v1/src/com/revivir/cementerio/MainLogin.java
+++ Cementerio_v1/src/com/revivir/cementerio/MainLogin.java
@@ -2,15 +2,7 @@ package com.revivir.cementerio;
 
 import java.util.Properties;
 
-import javax.swing.UIManager;
-
-<<<<<<< HEAD
-//import com.jtattoo.plaf.acryl.AcrylLookAndFeel;//1
-import com.revivir.cementerio.vista.login.ControladorIniciarSesion;
-=======
-import com.jtattoo.plaf.acryl.AcrylLookAndFeel;
 import com.revivir.cementerio.vista.sesion.ControladorIniciarSesion;
->>>>>>> refs/remotes/origin/BenDesarrollo
 
 public class MainLogin {
 
