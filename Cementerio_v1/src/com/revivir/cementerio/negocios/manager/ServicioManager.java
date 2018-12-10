2040cf1796d5094051eee8d0b453d489c7737415
Carlos <carloscaballeromorel@gmail.com>
2018-12-10 20:01:01 -0300

    Ventana servicio AM.

---
+++ Cementerio_v1/src/com/revivir/cementerio/negocios/manager/ServicioManager.java
@@ -0,0 +1,26 @@
+package com.revivir.cementerio.negocios.manager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.revivir.cementerio.persistencia.entidades.Servicio;
+
+public class ServicioManager {
+	
+	public static void guardar() {
+		
+	}
+	
+	public static void modificar(Servicio servicio) {
+		
+	}
+	
+	public static void eliminar(Servicio servicio) {
+		
+	}
+	
+	public static List<Servicio> traerTodo() {
+		return new ArrayList<>();
+	}
+
+}
