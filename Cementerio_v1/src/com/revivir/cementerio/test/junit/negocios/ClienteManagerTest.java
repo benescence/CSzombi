3e9e11645a98507cb6ada3130467a5d294268098
Carlos <carloscaballeromorel@gmail.com>
2018-12-13 00:57:29 -0300

    JUnit con negocios.

--- Cementerio_v1/src/com/revivir/cementerio/test/junit/negocios/ClienteManagerTest.java
+++ Cementerio_v1/src/com/revivir/cementerio/test/junit/negocios/ClienteManagerTest.java
@@ -1,51 +1,53 @@
-package com.revivir.cementerio.test.junit;
+package com.revivir.cementerio.test.junit.negocios;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+
+import com.revivir.cementerio.negocios.manager.ClienteManager;
 import com.revivir.cementerio.persistencia.FactoryOBD;
 import com.revivir.cementerio.persistencia.entidades.Cliente;
 import com.revivir.cementerio.persistencia.interfaces.ClienteOBD;
 
-class ClienteOBDTest {
-	private Cliente objeto = crearObjetoDePrueba();
+class ClienteManagerTest {
+	private String nombre = "N", apellido = "A", dni = "0", telefono = "1", email = "e";
 	private ClienteOBD obd = FactoryOBD.crearClienteOBD();
 	
-	private Cliente crearObjetoDePrueba() {
-		return new Cliente(-1, "000", "Apellido", "Nombre", "Telefono", "Email");
-	}
-
 	@Test
-	void testUpdate() {
-		obd.insert(objeto);
+	void testGuardar() {
+		Cliente local = new Cliente(-1, dni, apellido, nombre, telefono, email);
+		ClienteManager.guardar(nombre, apellido, dni, telefono, email);
+		Cliente clienteBD = obd.ultimoInsertado();
+		iguales(clienteBD, local);
+		obd.delete(clienteBD);
+	}
+	
+	@Test
+	void testModificar() {
+		ClienteManager.guardar(nombre, apellido, dni, telefono, email);
 		Cliente clienteBD1 = obd.ultimoInsertado();
+		
 		clienteBD1.setNombre("Prueba1");
 		clienteBD1.setApellido("Prueba2");
 		clienteBD1.setDni("Prueba3");
 		clienteBD1.setTelefono("Prueba4");
 		clienteBD1.setEmail("Prueba5");
-		obd.update(clienteBD1);
+		
+		ClienteManager.modificar(clienteBD1);
 		Cliente clienteBD2 = obd.ultimoInsertado();
+		
 		iguales(clienteBD1, clienteBD2);
 		obd.delete(clienteBD1);
 	}
 	
 	@Test
-	void testDelete() {
-		obd.insert(objeto);
-		Cliente clienteBD = obd.ultimoInsertado();
-		obd.delete(clienteBD);
-		clienteBD = obd.ultimoInsertado();
-		distintos(objeto, clienteBD);
+	void testEliminar() {
+		ClienteManager.guardar(nombre, apellido, dni, telefono, email);
+		Cliente clienteBD1 = obd.ultimoInsertado();		
+		ClienteManager.eliminar(clienteBD1);
+		Cliente clienteBD2 = obd.ultimoInsertado();
+		distintos(clienteBD1, clienteBD2);
 	}
-
-	@Test
-	void testUltimoInsertado() {
-		obd.insert(objeto);
-		Cliente clienteBD = obd.ultimoInsertado();
-		iguales(objeto, clienteBD);
-		obd.delete(clienteBD);
-	}	
 
 	private void iguales(Cliente c1, Cliente c2) {
 		assertTrue(c1.getNombre().equals(c2.getNombre()));
